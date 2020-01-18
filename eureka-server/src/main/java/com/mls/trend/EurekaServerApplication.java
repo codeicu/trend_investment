04408f20e81daf5e8bdda4ad210a7627b60483c9
马力神 <1607020421@s.upc.edu.cn>
2020-01-18 15:12:46 +0800

    微服务：注册中心

---
+++ eureka-server/src/main/java/com/mls/trend/EurekaServerApplication.java
@@ -0,0 +1,25 @@
+package com.mls.trend;
+
+import cn.hutool.core.util.NetUtil;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args){
+        /*
+        * 默认端口：8761
+        * 判断端口是否被占用，方便调试
+        * */
+        int port=8761;
+        if (!NetUtil.isUsableLocalPort(port)){
+            System.err.printf("端口%d被占用了，无法启动%n",port);
+            System.exit(1);
+        }
+        new SpringApplicationBuilder(EurekaServerApplication.class).properties("server.port=" + port).run(args);
+
+
+    }
+}
