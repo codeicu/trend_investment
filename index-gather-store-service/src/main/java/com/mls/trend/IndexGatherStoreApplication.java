54ab1c5b6ff2c6a04c0a36fab54ad7baca24ff54
马力神 <1607020421@s.upc.edu.cn>
2020-01-19 11:16:11 +0800

    数据采集存储：断路器

--- index-gather-store-service/src/main/java/com/mls/trend/IndexGatherStoreApplication.java
+++ index-gather-store-service/src/main/java/com/mls/trend/IndexGatherStoreApplication.java
@@ -8,11 +8,13 @@ import cn.hutool.core.util.StrUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class IndexGatherStoreApplication {
     public static void main(String[] args){
         int port=0;
--- index-gather-store-service/src/main/java/com/mls/trend/service/IndexService.java
+++ index-gather-store-service/src/main/java/com/mls/trend/service/IndexService.java
@@ -1,6 +1,8 @@
 package com.mls.trend.service;
 
+import cn.hutool.core.collection.CollectionUtil;
 import com.mls.trend.entity.Index;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,11 +21,22 @@ public class IndexService {
     @Autowired
     RestTemplate restTemplate;
 
+
+    @HystrixCommand(fallbackMethod = "third_paet_not_connected")
     public List<Index> fetch_indexes_from_third_part(){
         List<Map> temp= restTemplate.getForObject("http://localhost:8090/indexes/codes.json",List.class);
         return map2Index(temp);
 
     }
+
+    public List<Index> third_paet_not_connected(){
+        System.out.println("Third part index service not connected!");
+        Index index = new Index();
+        index.setCode("000000");
+        index.setName("无效指数代码");
+        return CollectionUtil.toList(index);
+    }
+
 
     private List<Index> map2Index(List<Map> temp) {
         List<Index> indexes =new ArrayList<>();
