8991b92ad5eb0332a3e404a158c717e63a84703a
马力神 <1607020421@s.upc.edu.cn>
2020-02-14 21:23:46 +0800

    断路器聚合监控

--- trend-trading-backtest-service/src/main/java/com/mls/trend/TrendTradingBackTestServiceApplication.java
+++ trend-trading-backtest-service/src/main/java/com/mls/trend/TrendTradingBackTestServiceApplication.java
@@ -9,6 +9,7 @@ import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -24,6 +25,7 @@ import java.util.concurrent.TimeoutException;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class TrendTradingBackTestServiceApplication {
     public static void main(String[] args) {
         int port = 0;
