e3a17ed10c31adddc3a567407158e2c834dcd44e
马力神 <1607020421@s.upc.edu.cn>
2020-02-21 17:40:38 +0800

    定时器修改

--- index-gather-store-service/src/main/java/com/mls/trend/config/MyQuartzConfiguration.java
+++ index-gather-store-service/src/main/java/com/mls/trend/config/MyQuartzConfiguration.java
@@ -2,12 +2,15 @@ package com.mls.trend.config;
 
 import com.mls.trend.util.IndexDataSyncJob;
 import org.quartz.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyQuartzConfiguration  {
-    private static final int interval=6;               //设置定时器刷新间隔(小时)
+
+    @Value("${myQuartzInterval}")
+    int interval;               //设置定时器刷新间隔(小时)
 
 //    注册定时器
     @Bean
@@ -20,7 +23,7 @@ public class MyQuartzConfiguration  {
     @Bean
     public Trigger weatherDataSyncTrigger(){
         SimpleScheduleBuilder scheduleBuilder=SimpleScheduleBuilder.simpleSchedule()
-                .withIntervalInHours(interval).repeatForever();    //每12小时刷新
+                .withIntervalInHours(interval).repeatForever();    //每interval小时刷新
         return TriggerBuilder.newTrigger().forJob(weatherDataSyncJobDetail())
                 .withIdentity("indexDataSyncTrigger").withSchedule(scheduleBuilder).build();
     }
